982829f52f814341c5f7deca313340eae08821a4
Jakub Kołuda <jko01@outlook.com>
2022-07-15 17:22:54 +0200

    add Select.js playerSelect() aiSelect()

---
+++ js/Select.js
@@ -0,0 +1,26 @@
+class Select {
+    constructor(hands) {
+        this.hands = hands;
+        this.choices = {
+            player: "",
+            ai: ""
+        }
+        this.playerSelect()
+    }
+    playerSelect() {
+        this.hands.forEach(hand => {
+            hand.addEventListener("click", () => {
+                this.hands.forEach(hand => {
+                    hand.style.border = "8px solid transparent"
+                })
+                hand.style.border = "8px solid black"
+                this.choices.player = hand.dataset.option;
+            })
+        });
+    }
+    aiSelect() {
+        const index = Math.floor(Math.random()*3);
+        this.choices.ai = this.hands[index].dataset.option;
+        
+    }
+}
--- js/game.js
+++ js/game.js
@@ -1,9 +1,6 @@
 class Game {
     constructor() {
-        this.rock = document.querySelector(".hands__rock");
-        this.paper = document.querySelector(".hands__paper");
-        this.scissors = document.querySelector(".hands__scissors");
-        this.buttonPlay = document.querySelector(".hands__button");
+        this.hands = [...document.querySelectorAll(".hand")];
         this.spanYourChoice = document.querySelector(".info__result-player-choice-span");
         this.spanAiChoice = document.querySelector(".info__result-ai-choice-span");
         this.spanWinner = document.querySelector(".info__result-winner-span");
@@ -11,8 +8,13 @@ class Game {
         this.spanWins = document.querySelector(".info__stats-wins-span");
         this.spanLosses = document.querySelector(".info__stats-losses-span");
         this.spanDraws = document.querySelector(".info__stats-draws-span");
+        this.select = new Select(this.hands);
+        document.querySelector(".hands__button").addEventListener("click", this.startGame.bind(this));
     }
     startGame() {
-
+        this.select.aiSelect();
+        console.log(this.select.choices);
+        
     }
+
 }
