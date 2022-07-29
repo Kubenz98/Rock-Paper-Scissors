bd77778fa78db608d9315abf058c32e7c4aeeb68
Jakub Kołuda <jko01@outlook.com>
2022-07-29 17:20:06 +0200

    change game.js add reset()

--- js/game.js
+++ js/game.js
@@ -20,8 +20,15 @@ class Game {
             console.log(result);
             this.result.publication(result, this.spanYourChoice, this.spanAiChoice, this.spanWinner);
             this.stats.update(result, this.spanGamesCount, this.spanWins, this.spanLosses, this.spanDraws);
+            this.reset();
         } else return alert("Select your hand!");
 
     }
-
+    reset() {
+        this.select.choices.ai = "";
+        this.select.choices.player = "";
+        this.hands.forEach(hand => {
+                hand.style.border = "8px solid transparent";
+        })
+    }
 }
